add739ffc0ac2840287b800edd34fa78b6b5f75c
Elias Baya <eliasbaya1223@gmail.com>
2025-03-25 02:17:27 +0300

    bugfix

--- src/payments/payments.module.ts
+++ src/payments/payments.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { PaymentsController } from './payments.controller';
 import { PaymentsService } from './payments.service';
 import { AirtelModule } from 'src/airtel/airtel.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AirtelModule],
+  imports: [AirtelModule, ConfigModule],
   controllers: [PaymentsController],
   providers: [PaymentsService],
   exports: [PaymentsService],
